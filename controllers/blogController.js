9fe022a372392e355019a909804401d520e2b486
ElioSpena <elio.spena.94@gmail.com>
2026-01-28 20:45:39 +0100

    feat: creta query delete e test postman

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -19,7 +19,12 @@ const update = (req, res) => {};
 const modify = (req, res) => {};
 
 const destroy = (req, res, next) => {
- 
+  const { id } = req.params;
+  const query = `DELETE FROM posts WHERE id = ?`;
+  connection.query(query, [id], (err) => {
+    if (err) return next(err);
+    res.sendStatus(204);
+  });
 };
 
 const blogController = { index, show, store, update, modify, destroy };
